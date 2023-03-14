ab5ea27981dea9489d58eee22f47fa2f7e0cd02a
pradita <praditame@gmail.com>
2023-03-14 16:46:34 +0700

    add 2 class examples

---
+++ src/Variables/Example1.java
@@ -0,0 +1,34 @@
+package Variables;
+
+public class Example1 {
+    public static void main (String[]args) {
+        //ini komentar untuk 1 baris
+        System.out.println("Hello world");
+
+        /* ini komentar untuk banyak baris
+        System.out.println("Ini belajar pertamaku");
+        System.out.println("MK Pemrograman Dasar");
+        System.out.println("Jam 07:30 - 10:50");
+        */
+
+
+        //contoh implementasi dari beberapa tipe data
+        int usia = 22; //integer cocok untuk data angka
+        float uangJajan = 20000; //float lebih cocok untuk data nominal uang
+        double luasPersegi = 20; //double lebih cocok digunakan untuk fungsi-fungsi matematika
+
+        char ch1 = 88; //char menggunakan unicode*. *Latin, Greek, kode ASCII, dsb.
+        char ch2 = 'Y';
+
+        boolean b = true;
+
+        System.out.println("Ini adalah hasil cetak usia " + usia );
+        System.out.println("Ini adalah hasil cetak uangJajan " + uangJajan );
+        System.out.println("Ini adalah hasil cetak luasPersegi " + luasPersegi );
+        System.out.println("Ini adalah hasil cetak ch1 " + ch1 );
+        System.out.println("Ini adalah hasil cetak ch2 " + ch2 );
+        System.out.println("Ini adalah hasil cetak b " + b );
+
+        System.out.println("Ini adalah hasil cetak dengan Character Escape \n" + usia + "\n" + uangJajan);
+    }
+}
---
+++ src/Variables/Example2.java
@@ -0,0 +1,31 @@
+package Variables;
+
+public class Example2 {
+    public static void main(String[] args) {
+        //implementasi variabel
+
+        String nama = "Pradita";
+        float dana1 = 10000, dana2 = 5000;
+        float hasilDana;
+
+        hasilDana = dana1 + dana2;
+
+        int a1=4, a2=2;
+        int hasilA = a1%a2; //modulus
+
+        System.out.println(hasilDana + "\n " + hasilA);
+
+        //relational operator
+        int b=1, c=2;
+        boolean d = b < c;
+        System.out.println(d);
+
+        //logical operator
+        boolean b1 = true;
+        boolean b2 = false;
+        boolean b3 = b1 | b2;
+        boolean b4 = b1 & b2;
+        System.out.println(b3);
+        System.out.println(b4);
+    }
+}
